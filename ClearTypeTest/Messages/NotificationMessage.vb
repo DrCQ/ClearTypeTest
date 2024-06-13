27a31d48dd73c2a2409b96708be6e2bd1e621c02
Christoph <dev@drcqsoft.com>
2024-06-13 16:46:40 +0200

    Add project files.

---
+++ ClearTypeTest/Messages/NotificationMessage.vb
@@ -0,0 +1,16 @@
+﻿Imports ClearTypeTest.Helper
+Imports CommunityToolkit.Mvvm.Messaging.Messages
+
+Namespace Messages
+
+    Friend Class NotificationMessage
+        Inherits ValueChangedMessage(Of AppNotification)
+
+        Sub New(arg As AppNotification)
+            MyBase.New(arg)
+        End Sub
+
+    End Class
+
+End Namespace
+
